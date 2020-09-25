09d0bfd41d1c6260ce8c3fa56b22b7cf798261a4
Alyssa Easterly <alyssa.easterly@yahoo.com>
2020-09-25 14:32:38 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,28 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  data.each_with_object({}) do |(key, value), final_array|
+    value.each do |inner_key, names |
+     names.each do |name|
+        if !final_array[name]
+          final_name[name] = {}
+      end
+        if !final_array[name][key]
+          !final_array[name][key] =[]
+        end
+        final_array[name][key].push(inner_key)
+         binding.pry
+       end
+    end
+    final_array
+  end
+
+ #data.fetch(:brown)
+ #data.fetch(:black)
+ 
+ #data.fetch
+
 end
+
+
