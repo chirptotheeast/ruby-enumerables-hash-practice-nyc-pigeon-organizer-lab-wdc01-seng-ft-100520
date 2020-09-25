e635f4fd6455757be7ab785542386d719f3ed038
Alyssa Easterly <alyssa.easterly@yahoo.com>
2020-09-25 14:45:29 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,7 +4,7 @@ def nyc_pigeon_organizer(data)
   # write your code here!
   final_results = data.each_with_object({}) do |(key, value), final_array|
     value.each do |inner_key, names |
-      binding.pry
+      #binding.pry
      names.each do |name|
         if !final_array[name]
           final_name[name] = {}
